de2ecd2ad03ac3ade33a883d1ec78824934da7bb
Rehan Ahmad <rahmad@poldit.com>
2021-11-05 17:55:46 -0400

    Removed backend code, only front end remains

--- graphql/loaders/loaders.d.ts
+++
@@ -1,9 +0,0 @@
-type BatchUser = (ids: readonly string[]) => Promise<IUser[]>;
-type BatchNotifications = (ids: readonly string[]) => Promise<INotification[]>;
-type BatchPolls = (ids: readonly string[]) => Promise<IPoll[]>;
-type BatchAnswers = (ids: readonly string[]) => Promise<IAnswer[]>;
-type BatchChats = (ids: readonly string[]) => Promise<IChat[]>;
-type BatchReplies = (ids: readonly string[]) => Promise<Ireply[]>;
-type BatchTopics = (ids: readonly string[]) => Promise<ITopic[]>;
-type BatchSubTopics = (ids: readonly string[]) => Promise<ISubTopic[]>;
-type BatchInternalUser = (ids: readonly string[]) => Promise<IinternalUsers[]>;
